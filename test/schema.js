e94b59b4ec5e40745c40cfec61f0fbe489697d08
Nebojsa Sabovic <nsabovic@gmail.com>
2013-04-19 16:46:27 -0700

    Report errors while loading.
    Now gives a path where the error happened, should help debug schema issues.

--- test/schema.js
+++ test/schema.js
@@ -12,14 +12,80 @@ try {
 sinon.assert.expose(global);
 
 describe('Schema', function() {
-  var s;
+  var s = null;
+
+  function loader(schemaObj) {
+    return function() {
+      s.load(schemaObj);
+    };
+  }
+
   beforeEach(function() {
     s = new Schema();
   });
 
   describe('load', function() {
-    it('loads a schema', function() {
+    it('loads a simple string schema', function() {
       assert(s.load('string'));
+    });
+    it('loads a simple record schema', function() {
+      var schema = {
+        "type" : "record",
+        "name" : "a",
+        "fields" : [ {
+          "name" : "b",
+          "type" : "int"
+        } ]
+      };
+      assert(s.load(schema));
+    });
+    it('loads a simple array schema', function() {
+      var schema = {
+        "type"  : "array",
+        "name"  : "a",
+        "items" : "int"
+      };
+      assert(s.load(schema));
+    });
+    it('loads a simple map schema', function() {
+      var schema = {
+        "type" : "map",
+        "name" : "a",
+        "keys" : "string",
+        "values": "boolean"
+      };
+      assert(s.load(schema));
+    });
+    it('prohibits longs in schema', function() {
+      assert.throws(loader('long'), Error);
+    });
+    it('prohibits unrecognized symbols in schema', function() {
+      assert.throws(loader('schlong'), Error);
+    });
+    it('prohibits unnames records in schema', function() {
+      var schema = {
+        "type" : "record",
+        "fields" : [ {
+          "name" : "b",
+          "type" : "string"
+        } ]
+      };
+      assert.throws(loader(schema), Error);
+    });
+    it('displays the path of failure in the error', function() {
+      var schema = {
+        "type" : "record",
+        "name" : "a",
+        "fields" : [ {
+          "name" : "b",
+          "type" : {
+            "type"  : "map",
+            "keys"  : "string",
+            "values": ["string", "blah"]
+          }
+        } ]
+      };
+      assert.throws(loader(schema), /root\.b\.values\:1/);
     });
   });
 });
